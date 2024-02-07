6b37c9761a79f0775978e62f10f129ed977c16f0
SentientUmaru <thetriggerin@gmail.com>
2024-02-07 15:27:29 -0800

    Adding Vial

---
+++ keyboards/keychron/v1_max/ansi_encoder/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xCA, 0x3F, 0x06, 0x52, 0xAA, 0x39, 0x36, 0xC7}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
